f7c6732685c8832a20f46e0d43f7d3167fe1c9f0
Louk-Chemdoc <lucasmessina83@hotmail.com>
2025-06-12 19:30:15 +0200

    Initialisation d'Airflow

---
+++ dags/pipeline.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime
+
+with DAG(
+    dag_id='pipeline_elt_docker',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['elt']
+) as dag:
+
+    elt_task = DockerOperator(
+        task_id='run_elt',
+        image='elt',  # Nom du service ou image Docker
+        container_name='elt_task_container',
+        command='python /app/script.py',
+        api_version='auto',
+        auto_remove=True,
+        docker_url='unix://var/run/docker.sock',
+        network_mode='data-pipeline'
+    )
+
+    dbt_task = DockerOperator(
+        task_id='run_dbt',
+        image='dbt',  # Nom du service ou image Docker
+        container_name='dbt_task_container',
+        command='dbt run',
+        api_version='auto',
+        auto_remove=True,
+        docker_url='unix://var/run/docker.sock',
+        network_mode='data-pipeline'
+    )
+
+    elt_task >> dbt_task
